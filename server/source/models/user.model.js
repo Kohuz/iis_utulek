369674d0140eb6847485bdf71f9ff434ae686dfb
Kryštof Albrecht <k@nymeria.tech>
2022-10-26 15:38:26 +0200

    Add model for User

---
+++ server/source/models/user.model.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("user", {
+    name: {
+      type: Sequelize.STRING,
+    },
+    surname: {
+      type: Sequelize.STRING,
+    },
+    birth_date: {
+      type: Sequelize.DATEONLY,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
+    bank_account: {
+      type: Sequelize.STRING,
+    },
+    address: {
+      type: Sequelize.STRING,
+    },
+    is_volunteer: {
+      type: Sequelize.BOOLEAN,
+    },
+    is_admin: {
+      type: Sequelize.BOOLEAN,
+    },
+    is_caretaker: {
+      type: Sequelize.BOOLEAN,
+    },
+    is_veterinarian: {
+      type: Sequelize.BOOLEAN,
+    },
+  });
+
+  return User;
+};
