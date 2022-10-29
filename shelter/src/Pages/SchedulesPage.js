37114604bfb53b2d612cedc97cded51c9dbd2222
Jakub Kozubek <jakub.kozubek@gmail.com>
2022-10-30 01:27:23 +0200

    shelter: start schedule component

--- shelter/src/Pages/SchedulesPage.js
+++ shelter/src/Pages/SchedulesPage.js
@@ -1,7 +1,82 @@
-import React from "react";
+import { Button } from "@mui/material";
+import React, { useState } from "react";
 
 function SchedulesPage() {
-  return <div>SchedulesPage</div>;
+  const days = ["Pondělí", "Úterý", "Středa"];
+  const hours = [
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
+  ];
+  let arr = [];
+  const [table, setTable] = useState([
+    {
+      day: "Pondělí",
+      hours: [
+        {
+          time: 1,
+          taken: false,
+        },
+        {
+          time: 2,
+          taken: false,
+        },
+      ],
+    },
+    {
+      day: "Úterý",
+      hours: [
+        {
+          time: 1,
+          taken: false,
+        },
+        {
+          time: 2,
+          taken: false,
+        },
+      ],
+    },
+  ]);
+  return (
+    <>
+      <table>
+        <caption>Hodiny</caption>
+        <tbody>
+          {table.map((day) => (
+            <tr>
+              <h5>{day.day}</h5>
+              {day.hours.map((hour, j) => (
+                <td>
+                  <Button
+                    variant="contained"
+                    color={hour.taken ? "error" : "primary"}
+                    onClick={() => {
+                      let newArr = [...table];
+                      let val = newArr
+                        .find((x) => x.day === day.day)
+                        .hours.find((y) => y.time === hour.time).taken;
+                      console.log(
+                        newArr
+                          .find((x) => x.day === day.day)
+                          .hours.find((y) => y.time === hour.time).taken
+                      );
+                      newArr
+                        .find((x) => x.day === day.day)
+                        .hours.find((y) => y.time === hour.time).taken = !val;
+                      setTable(newArr);
+                    }}
+                  >
+                    {hour.time}
+                  </Button>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Button onClick={() => console.log(table)}>d</Button>
+    </>
+  );
 }
 
 export default SchedulesPage;
