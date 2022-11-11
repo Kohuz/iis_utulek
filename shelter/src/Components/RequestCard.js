89328da7a8a2195da1449cc5b2dd605e4926738a
Jakub Kozubek <xkozub07@vutbr.cz>
2022-11-12 00:26:57 +0100

    shelter:new schedule stuff, request cards

---
+++ shelter/src/Components/RequestCard.js
@@ -0,0 +1,41 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function RequestCard({ request }) {
+  const navigate = useNavigate();
+  return (
+    <Card sx={{ maxWidth: 700 }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Zvíře: {request.animal}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+          Autor: {request.author}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Datum: {request.date}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          size="small"
+          onClick={() => navigate("/veteranian/event/" + request.id)}
+        >
+          Naplánovat událost
+        </Button>
+
+        <Button size="small">Zamítnout</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default RequestCard;
---
+++ shelter/src/Helpers/temp_arrays.js
@@ -0,0 +1,178 @@
+let timetable = [
+  {
+    day: "Pondělí",
+    hours: [
+      {
+        time: 8,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 9,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 10,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 11,
+        walk: false,
+        event: false,
+      },
+    ],
+  },
+  {
+    day: "Úterý",
+    hours: [
+      {
+        time: 8,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 9,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 10,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 11,
+        walk: false,
+        event: false,
+      },
+    ],
+  },
+  {
+    day: "Středa",
+    hours: [
+      {
+        time: 8,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 9,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 10,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 11,
+        walk: false,
+        event: false,
+      },
+    ],
+  },
+  {
+    day: "Čtvrtek",
+    hours: [
+      {
+        time: 8,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 9,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 10,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 11,
+        walk: false,
+        event: false,
+      },
+    ],
+  },
+  {
+    day: "Pátek",
+    hours: [
+      {
+        time: 8,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 9,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 10,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 11,
+        walk: false,
+        event: false,
+      },
+    ],
+  },
+  {
+    day: "Sobota",
+    hours: [
+      {
+        time: 8,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 9,
+        walk: false,
+        event: true,
+      },
+      {
+        time: 10,
+        walk: false,
+        event: true,
+      },
+      {
+        time: 11,
+        walk: false,
+        event: false,
+      },
+    ],
+  },
+  {
+    day: "Neděle",
+    hours: [
+      {
+        time: 8,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 9,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 10,
+        walk: false,
+        event: false,
+      },
+      {
+        time: 11,
+        walk: false,
+        event: false,
+      },
+    ],
+  },
+];
+export default timetable;
