45184cc9a989b180bc5a2b9ea95a5e44b2dc0062
Daniel Kříž <Daniel.kriz@protonmail.com>
2022-11-18 14:24:05 +0100

    schema: fix referencing of non-existent table
    Tables have to be defined, before they can be referenced. Therefore we
    have to split some of them into main definition and alteration later.

--- schema/00-schema.sql
+++ schema/00-schema.sql
@@ -11,6 +11,38 @@ DROP TABLE IF EXISTS animal_event;
 DROP TABLE IF EXISTS animal_request;
 DROP TABLE IF EXISTS requests;
 
+
+
+-- Event related tables
+
+CREATE TABLE events (
+    event_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    -- NOTE: in javascript models use DATE() filter function
+    date DATETIME,
+    commentary VARCHAR(1024) DEFAULT "",
+    type VARCHAR(255) DEFAULT "",
+    start DATETIME,
+    stop DATETIME,
+    state VARCHAR(255) DEFAULT "pending",
+    PRIMARY KEY (event_id)
+);
+
+
+
+-- Request related tables
+
+CREATE TABLE requests (
+    request_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    date DATETIME,
+    commentary VARCHAR(1024) DEFAULT "",
+    type VARCHAR(255) DEFAULT "",
+    -- NOTE: valid values are pending|approved|rejected
+    state VARCHAR(255) DEFAULT "pending",
+    PRIMARY KEY (request_id)
+);
+
+
+
 -- User tables
 
 CREATE TABLE users (
@@ -27,7 +59,16 @@ CREATE TABLE users (
     PRIMARY KEY (user_id)
 );
 
--- User relational tables
+ALTER TABLE events ADD (
+    -- AUTO_INCREMENTS guarantees, that ID values are going to start at 1
+    user_id INTEGER UNSIGNED DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+ALTER TABLE requests ADD (
+    user_id INTEGER UNSIGNED DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
 CREATE TABLE user_event (
     user_id INTEGER UNSIGNED NOT NULL,
@@ -40,29 +81,11 @@ CREATE TABLE user_event (
 CREATE TABLE user_request (
     user_id INTEGER UNSIGNED NOT NULL,
     request_id INTEGER UNSIGNED NOT NULL,
-    PRIMARY KEY (user_id, event_id),
+    PRIMARY KEY (user_id, request_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (request_id) REFERENCES requests(request_id)
 );
 
--- Event related tables
-
-CREATE TABLE events (
-    event_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-    -- NOTE: in javascript models use DATE() filter function
-    date DATETIME,
-    commentary VARCHAR(1024) DEFAULT "",
-    type VARCHAR(255) DEFAULT "",
-    start DATETIME,
-    stop DATETIME,
-    state VARCHAR(255) DEFAULT "pending",
-    -- AUTO_INCREMENTS guarantees, that ID values are going to start at 1
-    user_id INTEGER UNSINGED DEFAULT 0,
-    animal_id INTEGER UNSINGED DEFAULT 0,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (animal_id) REFERENCES animals(animals_id),
-    PRIMARY KEY (event_id)
-);
 
 
 -- Animal related tables
@@ -79,7 +102,15 @@ CREATE TABLE animals (
     PRIMARY KEY (animal_id)
 );
 
--- Animal relational tables
+ALTER TABLE events ADD (
+    animal_id INTEGER UNSIGNED DEFAULT 0,
+    FOREIGN KEY (animal_id) REFERENCES animals(animal_id)
+);
+
+ALTER TABLE requests ADD (
+    animal_id INTEGER UNSIGNED DEFAULT 0,
+    FOREIGN KEY (animal_id) REFERENCES animals(animal_id)
+);
 
 CREATE TABLE animal_event (
     animal_id INTEGER UNSIGNED NOT NULL,
@@ -92,25 +123,7 @@ CREATE TABLE animal_event (
 CREATE TABLE animal_request (
     animal_id INTEGER UNSIGNED NOT NULL,
     request_id INTEGER UNSIGNED NOT NULL,
-    PRIMARY KEY (animal_id, event_id),
+    PRIMARY KEY (animal_id, request_id),
     FOREIGN KEY (animal_id) REFERENCES animals(animal_id),
     FOREIGN KEY (request_id) REFERENCES requests(request_id)
-);
-
-
--- Request related tables
-
-CREATE TABLE requests (
-    request_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-    date DATETIME,
-    commentary VARCHAR(1024) DEFAULT "",
-    type VARCHAR(255) DEFAULT "",
-    -- NOTE: valid values are pending|approved|rejected
-    state VARCHAR(255) DEFAULT "pending",
-    PRIMARY KEY (request_id),
-    user_id INTEGER UNSINGED DEFAULT 0,
-    animal_id INTEGER UNSINGED DEFAULT 0,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (animal_id) REFERENCES animals(animals_id),
-    PRIMARY KEY (event_id)
 );
