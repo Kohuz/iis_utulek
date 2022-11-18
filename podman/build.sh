de41a69d5e3eb70581e71c05ab8d8091764b040d
Daniel Kříž <Daniel.kriz@protonmail.com>
2022-11-18 14:25:00 +0100

    podman: build: add newlines for better visual separation

--- podman/build.sh
+++ podman/build.sh
@@ -2,7 +2,10 @@
 podman_dir=$(realpath $(dirname $0))
 cd "$podman_dir/../schema"
 podman build -f ../podman/database/Containerfile -t shelter_db .
+printf "\n"
 
 cd "$podman_dir/.."
 podman build -f podman/frontend/Containerfile -t shelter_frontend .
+printf "\n"
+
 podman build -f podman/backend/Containerfile -t shelter_backend .
