f9a631974aaa65e3aa6a1cae43f7b512cc9f236a
Daniel Kříž <Daniel.kriz@protonmail.com>
2022-11-18 14:42:52 +0100

    schema: fill: add initial values

---
+++ schema/01-fill_with_data.sql
@@ -0,0 +1,30 @@
+USE shelter;
+
+INSERT INTO users (name, surname, password, bank_acc_no, is_caretaker)
+VALUES
+    ("Petr", "Pečovatel", "123456", "000", true);
+
+
+INSERT INTO users (name, surname, password, is_volunteer)
+VALUES
+    ("Dan", "Dobrovolný", "another", true),
+    ("Dora", "Dobrovolná", "another", true);
+
+
+INSERT INTO users (name, surname, password, bank_acc_no, is_veterinarian)
+VALUES
+    ("Václav","Veterinář", "doktor123", "001", true),
+    ("Zdeněk","Zvěrolékař", "123doktro", "002", true);
+
+INSERT INTO users (name, surname, password, bank_acc_no, is_admin)
+VALUES
+    ("Adam", "Ajťákovič", "random", "003", true);
+
+
+INSERT INTO animals (name, type, age)
+VALUES
+    ("Žofie", "Leguán", "3"),
+    ("Alík", "Aligátor", "12"),
+    ("Adam", "Pes", "5"),
+    ("Izidora", "Kočka", "1"),
+    ("Akuma", "Kočka", "3");
