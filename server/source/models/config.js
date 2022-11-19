a7905332a989b18461b724d5ae9812b4e4b174e5
Daniel Kříž <Daniel.kriz@protonmail.com>
2022-11-19 16:46:57 +0100

    server: models: config: add port option
    We are using custom port number, as we do not want to conflict with
    mysql servers on work stations.

--- server/source/models/config.js
+++ server/source/models/config.js
@@ -1,10 +1,12 @@
 module.exports = {
   HOST: 'localhost',
+  PORT: 6603,
   USER: 'root',
   PASSWORD: 'test',
   DB: 'utulek',
     host: HOST,
   options: {
+    port: 6603,
     dialect: 'mysql',
     operatorsAliases: 0,
     pool: {
