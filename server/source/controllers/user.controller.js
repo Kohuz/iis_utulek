d04832bb31eae653e4f912dabfe89cc7b82937bd
Daniel Kříž <Daniel.kriz@protonmail.com>
2022-11-20 00:40:47 +0100

    server: controllers: user: remove old example

--- server/source/controllers/user.controller.js
+++ server/source/controllers/user.controller.js
@@ -4,12 +4,4 @@ exports.create = (req, res) => {
   console.log('nothing');
 };
 
-exports.testRequest = (req, res) => {
-  const user = db.User();
-  const payloadObject = {
-    test: 'Test',
-    testNum: 42,
-  };
-  console.log('user: calling GET');
-  res.send(user);
 };
