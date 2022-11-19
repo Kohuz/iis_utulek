c6a6c64a97ec653482522f8685793b9c0d02c7ae
Daniel Kříž <Daniel.kriz@protonmail.com>
2022-11-19 16:47:44 +0100

    server: models: config: reference this hostname

--- server/source/models/config.js
+++ server/source/models/config.js
@@ -4,8 +4,8 @@ module.exports = {
   USER: 'root',
   PASSWORD: 'test',
   DB: 'utulek',
-    host: HOST,
   options: {
+    host: this.HOST,
     port: 6603,
     dialect: 'mysql',
     operatorsAliases: 0,
