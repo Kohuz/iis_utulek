be0a5757cff70060dc694eca59660da18257e79d
Daniel Kříž <Daniel.kriz@protonmail.com>
2022-11-19 16:49:10 +0100

    server: models: index: initialize models instances

--- server/source/models/index.js
+++ server/source/models/index.js
@@ -18,4 +18,16 @@ const database = {
 // const SomethingModel = require('./something.model.js');
 // database.something = SomethingModel(server, Sequelize);
 
+const UserModel = require('./user.model');
+database.user = UserModel(database.instance, database.driver);
+
+const EventModel = require('./event.model');
+database.event = EventModel(database.instance, database.driver);
+
+const AnimalModel = require('./animal.model');
+database.animal = AnimalModel(database.instance, database.driver);
+
+const RequestModel = require('./request.model');
+database.request = RequestModel(database.instance, database.driver);
+
 module.exports = database;
