bb0ca3b11f8f46ce4abd67a8fa47200185bcce2b
Daniel Kříž <Daniel.kriz@protonmail.com>
2022-11-19 16:51:58 +0100

    server: models: user: remove timestamp and reformat

--- server/source/models/user.model.js
+++ server/source/models/user.model.js
@@ -1,40 +1,45 @@
 module.exports = (sequelize, Sequelize) => {
-  const User = sequelize.define('user', {
-    name: {
-      type: Sequelize.STRING,
+  const User = sequelize.define(
+    'user',
+    {
+      name: {
+        type: Sequelize.STRING,
+      },
+      surname: {
+        type: Sequelize.STRING,
+      },
+      birth_date: {
+        type: Sequelize.DATEONLY,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      bank_account: {
+        type: Sequelize.STRING,
+      },
+      address: {
+        type: Sequelize.STRING,
+      },
+      is_volunteer: {
+        type: Sequelize.BOOLEAN,
+      },
+      is_admin: {
+        type: Sequelize.BOOLEAN,
+      },
+      is_caretaker: {
+        type: Sequelize.BOOLEAN,
+      },
+      is_veterinarian: {
+        type: Sequelize.BOOLEAN,
+      },
+      // For volunteers, that they are verified
+      verified: {
+        type: Sequelize.BOOLEAN,
+      },
     },
-    surname: {
-      type: Sequelize.STRING,
-    },
-    birth_date: {
-      type: Sequelize.DATEONLY,
-    },
-    password: {
-      type: Sequelize.STRING,
-    },
-    bank_account: {
-      type: Sequelize.STRING,
-    },
-    address: {
-      type: Sequelize.STRING,
-    },
-    is_volunteer: {
-      type: Sequelize.BOOLEAN,
-    },
-    is_admin: {
-      type: Sequelize.BOOLEAN,
-    },
-    is_caretaker: {
-      type: Sequelize.BOOLEAN,
-    },
-    is_veterinarian: {
-      type: Sequelize.BOOLEAN,
-    },
-    // For volunteers, that they are verified
-    verified: {
-      type: Sequelize.BOOLEAN,
-    },
-  });
+    {
+      timestamps: false,
+    }
   );
   return User;
 };
