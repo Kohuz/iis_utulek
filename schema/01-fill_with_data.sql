6c2f349227091cdb3be83455a2c0dca9e4dbae9a
Daniel Kříž <Daniel.kriz@protonmail.com>
2022-11-26 17:15:10 +0100

    schema: fill: reformat inserts

--- schema/01-fill_with_data.sql
+++ schema/01-fill_with_data.sql
@@ -2,23 +2,65 @@ USE shelter;
 
 INSERT INTO users (email, name, surname, password, bank_account, is_caretaker)
 VALUES
-    ("petr@test.test", "Petr", "Pečovatel", "123456", "000", true);
+    (
+        "petr@test.test",
+        "Petr",
+        "Pečovatel",
+        "123456",
+        "000",
+        true
+    );
 
 
 INSERT INTO users (email, name, surname, password, is_volunteer)
 VALUES
-    ("dobrovolny@test.test", "Dan", "Dobrovolný", "another", true),
-    ("dobrovolna@test.test", "Dora", "Dobrovolná", "another", true);
+    (
+        "dobrovolny@test.test",
+        "Dan",
+        "Dobrovolný",
+        "another",
+        true
+    ),
+    (
+        "dobrovolna@test.test",
+        "Dora",
+        "Dobrovolná",
+        "another",
+        true
+    );
 
 
 INSERT INTO users (email, name, surname, password, bank_account, is_veterinarian)
 VALUES
-    ("doctor1@veterinar-test.test", "Václav","Veterinář", "doktor123", "001", true),
-    ("doctor2@veterinar-test.test", "Zdeněk","Zvěrolékař", "123doktro", "002", true);
+    (
+        "doctor1@veterinar-test.test",
+        "Václav",
+        "Veterinář",
+        "doktor123",
+        "001",
+        true
+    ),
+    (
+        "doctor2@veterinar-test.test",
+        "Zdeněk",
+        "Zvěrolékař",
+        "123doktro",
+        "002",
+        true
+    );
+
 
 INSERT INTO users (email, name, surname, password, bank_account, is_admin, is_veterinarian)
 VALUES
-    ("adam.admin@test.test", "Adam", "Ajťákovič", "random", "003", true, true);
+    (
+        "adam.admin@test.test",
+        "Adam",
+        "Ajťákovič",
+        "random",
+        "003",
+        true,
+        true
+    );
 
 
 INSERT INTO animals (name, type, age)
